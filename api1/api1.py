d8893fc00cce358fc5c1ee7ff361cd96950dd1ac
gardnervile <lordy1jo@gmail.com>
2024-11-20 21:42:54 +0500

    Add files via upload

---
+++ api1/api1.py
@@ -0,0 +1,12 @@
+import requests
+
+
+params = {'lang': 'ru', 'nTqum': ''}
+url_template = 'http://wttr.in/{}'
+places = ['Лондон', "Шереметьево", "Череповец"]
+
+for place in places:
+    url = url_template.format(place)
+    response = requests.get(url, params=params)
+    print(response.url) 
+print(response.text)
