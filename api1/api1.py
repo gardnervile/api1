45bd36ecfd66c5a33fe167f7455c7b52df641af3
gardnervile <lordy1jo@gmail.com>
2024-11-20 21:55:05 +0500

    Update api1.py

--- api1/api1.py
+++ api1/api1.py
@@ -3,10 +3,10 @@ import requests
 
 params = {'lang': 'ru', 'nTqum': ''}
 url_template = 'http://wttr.in/{}'
-places = ['Лондон', "Шереметьево", "Череповец"]
+places = ['Лондон', 'Шереметьево', 'Череповец']
 
 for place in places:
     url = url_template.format(place)
     response = requests.get(url, params=params)
-    print(response.url) 
-print(response.text)
+    print(response.url)
+    print(response.text)
