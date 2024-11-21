5c0a966570143969e0a5ec382be9de58d4b8336c
gardnervile <lordy1jo@gmail.com>
2024-11-21 22:58:49 +0500

    Update api1.py

--- api1/api1.py
+++ api1/api1.py
@@ -1,12 +1,32 @@
 import requests
+import logging
 
 
-params = {'lang': 'ru', 'nTqum': ''}
-url_template = 'http://wttr.in/{}'
-places = ['Лондон', 'Шереметьево', 'Череповец']
+def configure_logging():
+    logging.basicConfig(level=logging.DEBUG)
 
-for place in places:
-    url = url_template.format(place)
-    response = requests.get(url, params=params)
-    print(response.url)
-    print(response.text)
+
+def fetch_weather_data():
+    params = {'lang': 'ru', 'nTqum': ''}
+    url_template = 'http://wttr.in/{}'
+    places = ['Лондон', 'Шереметьево', 'Череповец']
+
+    for place in places:
+        try:
+            url = url_template.format(place)
+            response = requests.get(url, params=params)
+            logging.debug(response.url)
+            response.raise_for_status()
+        except requests.exceptions.RequestException as err:
+            print(f'Error occurred: {err}')
+        else:
+            logging.debug(response.text)
+
+
+def main():
+    configure_logging()
+    fetch_weather_data()
+
+
+if __name__ == "__main__":
+    main()
