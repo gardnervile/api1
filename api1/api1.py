5dc0165ca18958669b406aa364dccef8e10fc91b
gardnervile <lordy1jo@gmail.com>
2024-11-22 22:00:16 +0500

    Update api1.py

--- api1/api1.py
+++ api1/api1.py
@@ -1,31 +1,25 @@
 import requests
-import logging
 
 
-def configure_logging():
-    logging.basicConfig(level=logging.DEBUG)
-
-
-def fetch_weather_data():
+def fetch_weather_data(place):
     params = {'lang': 'ru', 'nTqum': ''}
-    url_template = 'http://wttr.in/{}'
-    places = ['Лондон', 'Шереметьево', 'Череповец']
+    url = f'http://wttr.in/{place}'
+    response = requests.get(url, params=params)
+    response.raise_for_status()
+    return response.text
 
-    for place in places:
-        try:
-            url = url_template.format(place)
-            response = requests.get(url, params=params)
-            logging.debug(response.url)
-            response.raise_for_status()
-        except requests.exceptions.RequestException as err:
-            print(f'Error occurred: {err}')
-        else:
-            logging.debug(response.text)
+
+def get_weather_safely(place):
+    try:
+        return fetch_weather_data(place)
+    except requests.exceptions.RequestException as err:
+        return f'Error occurred while fetching weather for {place}: {err}'
 
 
 def main():
-    configure_logging()
-    fetch_weather_data()
+    places = ['Лондон', 'Шереметьево', 'Череповец']
+    for place in places:
+        print(f'Weather data for {place}:\n{get_weather_safely(place)}\n')
 
 
 if __name__ == "__main__":
